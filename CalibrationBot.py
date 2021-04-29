de0fc93fe4662a90434a83dc849bb46bb6e84158
NeS1F <star3pro@gmail.com>
2021-04-29 18:58:07 +0300

    'перший_пуск'

---
+++ CalibrationBot.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot('1713086812:AAHhAQEOyZ8FXHPmlf3O8D9yVFN1iaGk3TA')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот для обчислення коефіцієнтів калібрування. Приємно познайомитися!, {message.from_user.first_name}')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет' or message.text.lower() == 'привіт' or message.text.lower() == 'hello':
+        bot.send_message(message.from_user.id, 'Привіт!')
+    else:
+        bot.send_message(message.from_user.id, 'Я ще маленький і вмійю тільки вітатися. Привіт =) ')
+bot.polling(none_stop=True)
