f1772629b15fc4c5b242f87e1111e66d8d2a3b8a
NeS1F <star3pro@gmail.com>
2021-05-11 13:39:53 +0300

    0.131

--- db.py
+++ db.py
@@ -2,6 +2,4 @@ import pymongo
  
 
 client = pymongo.MongoClient("mongodb+srv://CalibBot:718293465calibbot@calibbotcluster.1rxhx.mongodb.net/CalibBotDatabase.users_db?retryWrites=true&w=majority")
-users_db = client.test
-
-#get_database()["users_db"]
+users_db = client.get_database()["users_db"]
